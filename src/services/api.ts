5de8bc4eca4028472606e5931825cf95d661135b
whshin <urihiya1@kaist.ac.kr>
2025-07-21 23:23:12 +0900

    tlqkf help

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,7 +11,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+  withCredentials: false, // 살려주세요
 });
 
 // JWT 토큰을 헤더에 추가하는 인터셉터
